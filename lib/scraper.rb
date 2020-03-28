46b8ddc4aeede136e3931b828afa970378011bd0
Aglae Verduzco <aglae.cv@gmail.com>
2020-03-28 22:36:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,20 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+      url = ("https://learn-co-curriculum.github.io/student-scraper-test-page")
+      website = Nokogiri::HTML(open(url))
+
+        website.css("div.roster-cards-container").each do |student|
+        student_hash = {
+          name: student.css("div.student-card").text,
+          location: student.css("div.student-location").text
+          #profile_url:
+        }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
